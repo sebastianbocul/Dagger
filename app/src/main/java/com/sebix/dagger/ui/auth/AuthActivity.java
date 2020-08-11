ee797bae88fff404eedaf8b1576e0d2614c1dc5d
sebastianbocul <seba23495@gmail.com>
2020-08-11 21:53:23 +0200

    Connected rxJava and LiveData

--- app/src/main/java/com/sebix/dagger/ui/auth/AuthActivity.java
+++ app/src/main/java/com/sebix/dagger/ui/auth/AuthActivity.java
@@ -1,23 +1,30 @@
 package com.sebix.dagger.ui.auth;
 
+import android.content.DialogInterface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.bumptech.glide.RequestManager;
 import com.bumptech.glide.request.RequestOptions;
 import com.sebix.dagger.R;
+import com.sebix.dagger.models.User;
 import com.sebix.dagger.viewmodels.ViewModelProviderFactory;
 
 import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
-public class AuthActivity extends DaggerAppCompatActivity {
+public class AuthActivity extends DaggerAppCompatActivity implements View.OnClickListener {
     private static final String TAG = "AuthActivity";
-
+    private EditText userId;
     private AuthViewModel viewModel;
 
     @Inject
@@ -34,13 +41,42 @@ public class AuthActivity extends DaggerAppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        userId=findViewById(R.id.user_id_input);
+        findViewById(R.id.login_button).setOnClickListener(this);
         viewModel = ViewModelProviders.of(this,providerFactory).get(AuthViewModel.class);
         setLogo();
+        subscribeObservers();
     }
 
     private void setLogo(){
         requestManager.load(logo)
                 .into((ImageView)findViewById(R.id.login_logo));
+    }
 
+    private void subscribeObservers(){
+        viewModel.observeUser().observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                if (user!=null) {
+                    Log.d(TAG, "onChanged: " + user.getEmail());
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login_button:
+                attemptLogin();
+                break;
+        }
+    }
+
+    private void attemptLogin() {
+        if(TextUtils.isEmpty(userId.getText().toString())){
+            return;
+        }
+        viewModel.authenticateWithId(Integer.parseInt(userId.getText().toString()));
     }
 }
--- app/src/main/java/com/sebix/dagger/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/sebix/dagger/ui/auth/AuthViewModel.java
@@ -2,6 +2,10 @@ package com.sebix.dagger.ui.auth;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.LiveDataReactiveStreams;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
 import com.sebix.dagger.models.User;
@@ -9,7 +13,6 @@ import com.sebix.dagger.network.auth.AuthApi;
 
 import javax.inject.Inject;
 
-import io.reactivex.Observer;
 import io.reactivex.Scheduler;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
@@ -18,37 +21,30 @@ public class AuthViewModel extends ViewModel {
     private static final String TAG = "AuthViewModel";
     private final AuthApi authApi;
 
+    private MediatorLiveData<User> authUser = new MediatorLiveData<>();
+
     @Inject
     public AuthViewModel(AuthApi authApi) {
         this.authApi=authApi;
         Log.d(TAG, "AuthViewModel: viewmodel is working ..");
-        if(this.authApi==null){
-            Log.d(TAG, "AuthViewModel: authApi is null ..");
-        }else {
-            Log.d(TAG, "AuthViewModel: authApi not null..");
-        }
-        authApi.getUser(1)
-                .toObservable()
-                .subscribeOn(Schedulers.io())
-                .subscribe(new Observer<User>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-                    }
-
-                    @Override
-                    public void onNext(User user) {
-                        Log.d(TAG, "onNext: " + user.getEmail());
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        Log.d(TAG, "onError: " + e);
-                    }
-
-                    @Override
-                    public void onComplete() {
-                    }
-                });
-
     }
+
+    public void authenticateWithId(int userId){
+        final LiveData<User> source = LiveDataReactiveStreams.fromPublisher(
+                authApi.getUser(userId)
+                .subscribeOn(Schedulers.io())
+        );
+        authUser.addSource(source, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                authUser.setValue(user);
+                authUser.removeSource(source);
+            }
+        });
+    }
+
+    public LiveData<User> observeUser(){
+        return authUser;
+    }
+
 }
