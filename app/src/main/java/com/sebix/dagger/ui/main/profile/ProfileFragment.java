e13e1ab75491ac593ede0c8e1f521519908c3594
sebastianbocul <seba23495@gmail.com>
2020-08-12 20:39:11 +0200

    Data is now loaded to textViews

--- app/src/main/java/com/sebix/dagger/ui/main/profile/ProfileFragment.java
+++ app/src/main/java/com/sebix/dagger/ui/main/profile/ProfileFragment.java
@@ -5,14 +5,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.sebix.dagger.R;
+import com.sebix.dagger.models.User;
+import com.sebix.dagger.ui.auth.AuthResource;
 import com.sebix.dagger.viewmodels.ViewModelProviderFactory;
 
 import javax.inject.Inject;
@@ -22,19 +24,59 @@ import dagger.android.support.DaggerFragment;
 public class ProfileFragment extends DaggerFragment {
     private static final String TAG = "ProfileFragment";
     private ProfileViewModel viewModel;
-
+    private TextView email,userName,website;
     @Inject
     ViewModelProviderFactory providerFactory;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile,container,false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         Log.d(TAG, "onViewCreated: ProfileFragment created");
-        viewModel = ViewModelProviders.of(getActivity(),providerFactory).get(ProfileViewModel.class);
+        viewModel = ViewModelProviders.of(getActivity(), providerFactory).get(ProfileViewModel.class);
+        email = view.findViewById(R.id.email);
+        userName = view.findViewById(R.id.username);
+        website = view.findViewById(R.id.website);
+        subscribeObservers();
+    }
+
+    private void subscribeObservers() {
+        viewModel.getAuthenticatedUser().removeObservers(getViewLifecycleOwner());
+        viewModel.getAuthenticatedUser().observe(getViewLifecycleOwner(), new Observer<AuthResource<User>>() {
+            @Override
+            public void onChanged(AuthResource<User> userAuthResource) {
+                if (userAuthResource != null){
+                    switch (userAuthResource.status){
+                        case AUTHENTICATED:{
+                            setUsersDetails(userAuthResource.data);
+                            break;
+                        }
+
+                        case ERROR:{
+                            setErrorDetails(userAuthResource.message);
+                            break;
+                        }
+                    }
+                }
+
+            }
+        });
+    }
+
+    private void setErrorDetails(String message) {
+        email.setText(message);
+        userName.setText("Error");
+        website.setText("Error");
+    }
+
+    private void setUsersDetails(User data) {
+        email.setText(data.getEmail());
+        userName.setText(data.getUsername());
+        website.setText(data.getWebsite());
+
     }
 }
--- app/src/main/java/com/sebix/dagger/ui/main/profile/ProfileViewModel.java
+++ app/src/main/java/com/sebix/dagger/ui/main/profile/ProfileViewModel.java
@@ -2,15 +2,25 @@ package com.sebix.dagger.ui.main.profile;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.sebix.dagger.SessionManager;
+import com.sebix.dagger.models.User;
+import com.sebix.dagger.ui.auth.AuthResource;
 
 import javax.inject.Inject;
 
 public class ProfileViewModel extends ViewModel {
     private static final String TAG = "ProfileViewModel";
-
+    private final SessionManager sessionManager;
     @Inject
-    public ProfileViewModel() {
+    public ProfileViewModel(SessionManager sessionManager) {
+        this.sessionManager=sessionManager;
         Log.d(TAG, "ProfileViewModel: viewmodel is ready");
+    }
+
+    public LiveData<AuthResource<User>> getAuthenticatedUser(){
+        return sessionManager.getAuthUser();
     }
 }
